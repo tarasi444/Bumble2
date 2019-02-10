5dc164a0c64c17d784f93f6c1fb896a2405b2e6b
dtr <tarasi44444@gmail.com>
2019-02-10 21:22:39 +0200

    Task12rt

--- src/OutPrint12.java
+++ src/OutPrint12.java
@@ -1,7 +1,8 @@
 public class OutPrint12 {
 
     public static void main (String[] args){
-
+        int a = 1;
+        this.''
 
         System.out.println("Test");
         System.out.println("Test");
