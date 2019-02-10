38633ea9c7e1aec4ad5468b1de88f2fbc739ec32
dtr <tarasi44444@gmail.com>
2019-02-10 21:31:39 +0200

    Task12rt

---
+++ src/test1/test/OutPrint1235.java
@@ -0,0 +1,15 @@
+package test1.test;{
+    "tasks": [
+        {
+            "label": "Hello",
+            "type": "shell",
+            "cwd": "${workspaceFolder}",
+            "command": "bash",
+            "args": [
+                    "-c",
+                    "echo 1 2 3"
+                    ]
+        }
+        }
+            ]
+        }
